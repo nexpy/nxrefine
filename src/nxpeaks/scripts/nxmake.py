3749b8ac22963020bbf63fa9b419094b482acccf
Ray Osborn <rayosborn@mac.com>
2015-03-27 16:07:44 -0500

    Refined nxmake to operate on a single directory
    Will create a single NeXus file based for a particular set of scans at,
    e.g., a single temperature.

--- src/nxpeaks/scripts/nxmake.py
+++ src/nxpeaks/scripts/nxmake.py
@@ -1,123 +1,100 @@
+import argparse
 import os
 import re
 import numpy as np
 import subprocess
-
 from nexusformat.nexus import *
 
-#home_dir = '/home/bessrc/sharedbigdata/data1/osborn-2014-1'
-#directory_pattern = re.compile('(.*)[kK]') 
-#directory_pattern = re.compile('(.*)_([0-9]*)[kK]')
-#raw_files = ['f1', 'f2'] 
-#raw_files = ['ff1scan', 'ff2scan', 'fb1scan', 'fb2scan', 'sfscan', 'sbscan', 'ubfscan', 'ubbscan']
-#pixel_mask = nxload(os.path.join(home_dir, 'pilatus_mask.nxs'))['entry/mask']
-#pixel_mask = None
 
-
-def make_nexus_file(scan_dir, sample_dir, filenames, pattern, specfile,
-                    label=False):
-    if label:
-        sample_label = parent(scan_dir)
-        sample_name = grandparent(scan_dir)
-    else:
-        sample = parent(scan_dir)
-    scan = os.path.basename(scan_dir)
-    nexus_file = os.path.join(sample_dir, sample_name + '_' + scan + '.nxs')
+def make_nexus_file(sample_name, sample_label, scan_dir, parameter, unit, 
+                    filenames, pattern,  mask=None):    
     root = NXroot()
     sample = NXsample()
     sample.name = sample_name
-    if label:
+    if sample_label:
         sample.label = sample_label
-    try:
-        sample.temperature = np.float32(re.match(pattern, scan).group(1))
-        sample.temperature.units='K'
-    except ValueError:
-        pass
+    if parameter is not None:
+        try:
+            scan = os.path.basename(scan_dir)
+            sample[parameter] = np.float32(re.match(pattern, scan).group(1))
+            sample[parameter].attrs['units'] = unit
+        except ValueError:
+            pass
     root.entry = NXentry(sample)
     for f in filenames:
         if f+'.nxs' in os.listdir(scan_dir):
-            root[f] = make_entry(os.path.join(scan_dir, f+'.nxs'))
+            root[f] = make_entry(os.path.join(scan_dir, f+'.nxs'), mask)
             root[f].makelink(root.entry.sample)
-            if specfile is not None:
-                subentries = import_spec_file(os.path.join(scan_dir, f, specfile))
-            for subentry in subentries:
-                root[f][subentry.nxname] = subentry
-    print nexus_file
-    root.save(nexus_file, 'w')
+    return root
     
-def make_entry(scan_file):
+
+def make_entry(scan_file, mask=None):
     root = nxload(scan_file)
     entry = NXentry(NXdata())
     entry.filename = root.entry.filename
     entry.start_time = root.entry.start_time
     entry.instrument = root.entry.instrument
-    if pixel_mask:
-        entry.instrument.detector.pixel_mask = pixel_mask
+    if mask is not None:
+        entry.instrument.detector.pixel_mask = mask
         entry.instrument.detector.pixel_mask_applied = False
     entry.data.x_pixel = root.entry.data.x_pixel
     entry.data.y_pixel = root.entry.data.y_pixel
     entry.data.frame_number = root.entry.data.frame_number
-    entry.data.data = NXlink(target='/entry/data/data', file=scan_file)
+    scan_parent = os.path.basename(os.path.dirname(scan_file))
+    scan_link = os.path.join(scan_parent, os.path.basename(scan_file))
+    entry.data.data = NXlink(target='/entry/data/data', file=scan_link)
     entry.data.nxsignal = entry.data.data
     entry.data.nxaxes = [entry.data.frame_number, entry.data.y_pixel, 
                          entry.data.x_pixel]
+    for subentry in root.entry.NXsubentry:
+        entry[subentry.nxname] = subentry
+    for process in root.entry.NXprocess:
+        entry[process.nxname] = process 
     return entry
 
-def import_spec_file(specfile):
-    subprocess.call('spec2nexus --quiet '+specfile, shell=True)
-    subentries = []
-    prefix = os.path.splitext(os.path.basename(specfile))[0]
-    directory = os.path.dirname(specfile)
-    try:
-        spec = nxload(os.path.join(directory, prefix+'.hdf5'))
-        for entry in spec.NXentry:
-            entry.nxclass = NXsubentry
-            subentries.append(entry)
-    except:
-        pass
-    return subentries
-
-def subdirs(dir):
-    return [os.path.join(dir,f) for f in os.listdir(dir) 
-            if os.path.isdir(os.path.join(dir,f))]
-
-def parent(dir):
-    return os.path.basename(os.path.dirname(dir))
-
-def grandparent(dir):
-    return os.path.basename(os.path.dirname(os.path.dirname(dir)))
 
 def main():
 
     parser = argparse.ArgumentParser(
         description="Create composite NeXus files")
-    parser.add_argument('-d', '--directory', default='./')
-    parser.add_argument('-p', '--pattern', default='(.*)[kK]')
-    parser.add_argument('-l', '--label', action='store_true')
-    parser.add_argument('-f', '--filenames', default=['f1', 'f2'])
-    parser.add_argument('-m', '--maskfile')
-    parser.add_argument('-s', '--specfile')
+    parser.add_argument('-s', '--sample', help='sample name')
+    parser.add_argument('-l', '--label', default='', help='sample label')
+    parser.add_argument('-d', '--directory', default='', help='scan directory')
+    parser.add_argument('-r', '--regular', default='(.*)[kK]',
+        help=('regular expression pattern for extracting the parameter value'
+              'from the scan directory'))
+    parser.add_argument('-p', '--parameter',
+        help='name of the varying parameter (assumed to be a sample parameter)')
+    parser.add_argument('-u', '--unit', default='K', help='parameter units')
+    parser.add_argument('-f', '--filenames', default=['f1', 'f2'], nargs='+',
+        help='names of merged NeXus files to be linked to this file')
+    parser.add_argument('-m', '--maskfile', default='pilatus_mask.nxs',
+        help='name of the pixel mask file')
 
     args = parser.parse_args()
 
+    sample_name = args.sample
+    sample_label = args.label
     directory = args.directory
+    pattern = re.compile(args.regular)
+    parameter = args.parameter
+    unit = args.unit
     filenames = args.filenames
-    pattern = re.compile(args.pattern)
-    specfile = args.specfile
-    label = args.label
-    mask nxload(os.path.join(directory, 'pilatus_mask.nxs'))['entry/mask']
+    if args.maskfile is not None:
+        mask = nxload(args.maskfile)['entry/mask']
+    else:
+        mask = None
 
-    for sample_name in subdirs(directory):
-        if label:
-            for sample_label in subdirs(os.path.join(directory, sample_name)):
-                sample_dir = os.path.join(directory, sample_name, sample_label)
-                for scan_dir in subdirs(sample_dir):
-                    make_nexus_file(scan_dir, sample_dir, args)
-        else:
-            sample_dir = os.path.join(directory, sample_name)
-            for scan_dir in subdirs(sample_dir):
-                make_nexus_file(scan_dir, sample_dir, args)
-
+    scan_dir = os.path.join(sample_name, sample_label, directory)
+    scan = os.path.basename(scan_dir)
+    if scan:
+        nexus_file = os.path.join(sample_name, sample_label, sample_name + '_' + scan + '.nxs')
+    else:
+        nexus_file = os.path.join(sample_name, sample_label, sample_name + '.nxs')
+    root = make_nexus_file(sample_name, sample_label, scan_dir, 
+                           parameter, unit, filenames, pattern, mask)
+    root.save(nexus_file, 'w')
+    print 'Saving ', nexus_file
 
 if __name__ == '__main__':
     main()
