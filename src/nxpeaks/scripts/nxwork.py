2c3276c52ea19b50a83d90972b783c6da78883f1
Justin M Wozniak <wozniak@mcs.anl.gov>
2015-04-29 11:38:53 -0500

    Use nxstack library

--- src/nxpeaks/scripts/nxwork.py
+++ src/nxpeaks/scripts/nxwork.py
@@ -1,5 +1,12 @@
+
+'''
+This script should be run with PWD as the top-level data directory
+'''
+
 import argparse, os, subprocess
 import numpy as np
+
+from nexusformat.tools.stack import nxstack
 
 def crash(msg):
     print msg
@@ -7,18 +14,20 @@ def crash(msg):
 
 def main():
 
+    print "nxwork..."
+
     parser = argparse.ArgumentParser(
         description="Perform workflow for scan")
     parser.add_argument('-s', '--sample', help='sample name')
     parser.add_argument('-l', '--label', default='', help='sample label')
     parser.add_argument('-d', '--directory', default='', help='scan directory')
     parser.add_argument('-t', '--temperature', help='temperature of scan')
-    parser.add_argument('-f', '--filenames', default=['f1', 'f2', 'f3'], 
+    parser.add_argument('-f', '--filenames', default=['f1', 'f2', 'f3'],
         nargs='+', help='names of NeXus files to be linked to this file')
     parser.add_argument('-m', '--maskfile', default='pilatus_mask.nxs',
         help='name of the pixel mask file')
     parser.add_argument('-p', '--parent', help='file name of file to copy from')
-    
+
     args = parser.parse_args()
 
     sample = args.sample
@@ -30,20 +39,23 @@ def main():
 
     label_path = '%s/%s' % (sample, label)
     wrapper_file = '%s/%s_%s.nxs' % (label_path, sample, directory)
+    print "wrapper file:", wrapper_file
 
     if sample == None:
         crash('Requires sample!')
     if not os.path.exists(label_path):
         crash("Label does not exist: "+label_path)
-    
+
     if not os.path.exists(wrapper_file):
         setup_command = 'nxsetup -s %s -l %s -d %s -t %s -f %s' \
                         % (sample, label, directory, temperature, ' '.join(files))
 
     for f in files:
         path = '%s/%s/%s/%s' % (sample, label, directory, f)
-        subprocess.call('nxstack -d %s -p scan -e cbf -o %s.nxs -c None'
-                        % (path, path))
+        print "calling nxstack"
+        nxstack(directory, prefixes=['scan'], extension='cbf',
+                output='%s.nxs'%output,
+        print "calling nxlink"
         subprocess.call('nxlink -s %s -l %s -d %s -f %s -m pilatus_mask.nxs'
                         % (sample, label, directory, f))
         subprocess.call('nxmax -d %s -f %s -p %s/data'
@@ -52,7 +64,7 @@ def main():
                         % (label_path, wrapper_file, f))
 
     if parent:
-        subprocess.call('nxcopy -f %s/%s -o %s' 
+        subprocess.call('nxcopy -f %s/%s -o %s'
                         % (label_path, parent, wrapper_file))
 
 if __name__=="__main__":
