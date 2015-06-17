dd63a256c4bd9c78ee67c0f33d8f91fd71964bbe
Ray Osborn <rayosborn@mac.com>
2015-06-17 17:05:50 -0500

    Various updates
    Mostly to use the select_entry function. Also, existing maxima are now
    displayed when an entry is selected.

--- src/plugins/refine/find_maximum.py
+++ src/plugins/refine/find_maximum.py
@@ -1,4 +1,4 @@
-from PySide import QtGui
+from nexpy.gui.pyqt import QtGui
 import numpy as np
 from nexpy.gui.datadialogs import BaseDialog
 from nexpy.gui.mainwindow import report_error
@@ -17,24 +17,28 @@ class MaximumDialog(BaseDialog):
 
     def __init__(self, parent=None):
         super(MaximumDialog, self).__init__(parent)
-        self.node = self.get_node()
-        self.entry = self.node.nxentry
-        if not isinstance(self.node, NXfield):
-            raise NeXusError('Node must be an NXfield')
+
+        self.select_entry(self.choose_entry)
+
         self.output = QtGui.QLabel('Maximum Value:')
-        self.find_button = QtGui.QPushButton('Find Maximum')
-        layout.addWidget(self.output)
-        layout.addWidget(self.find_button)
-        self.find_button.clicked.connect(self.find_maximum)
-        if len(self.node.shape) == 2:
-            layout.addWidget(self.close_buttons(save=True))
-        elif len(self.node.shape) > 2:
-            layout.addLayout(self.progress_layout(save=True))
-            self.progress_bar.setVisible(False)
-        self.set_layout(layout)
+        action_buttons = self.action_buttons(('Find Maximum', self.find_maximum))
+        self.set_layout(self.entry_layout, self.output, 
+                        action_buttons, self.progress_layout(save=True))
+        self.progress_bar.setVisible(False)
         self.set_title('Find Maximum Value')
 
+    def choose_entry(self):
+        try:
+            self.output_maximum(self.entry['data'].attrs['maximum'])
+        except Exception:
+            self.output_maximum('')
+
+    def output_maximum(self, maximum):
+        self.output.setText('Maximum Value: %s' % maximum)
+
     def find_maximum(self):
+        if 'data' not in self.entry:
+            raise NeXusError('There must be a group named "data" in the entry')
         self.maximum = 0.0
         try:
             mask = self.entry['instrument/detector/pixel_mask']
@@ -42,27 +46,28 @@ class MaximumDialog(BaseDialog):
                 mask = mask[0]
         except Exception:
             mask = None
-        if len(self.node.shape) == 2:
-            self.maximum = self.node[:,:].max()
+        signal = self.entry['data'].nxsignal
+        if len(signal.shape) == 2:
+            self.maximum = signal[:,:].max()
         else:
             self.progress_bar.setVisible(True)
-            self.progress_bar.setRange(0, self.node.shape[0])
-            chunk_size = self.node.nxfile[self.node.nxpath].chunks[0]
-            for i in range(0, self.node.shape[0], chunk_size):
+            self.progress_bar.setRange(0, signal.shape[0])
+            chunk_size = signal.nxfile[signal.nxpath].chunks[0]
+            for i in range(0, signal.shape[0], chunk_size):
                 try:
                     self.progress_bar.setValue(i)
                     self.update_progress()
-                    v = self.node[i:i+chunk_size,:,:]
+                    v = signal[i:i+chunk_size,:,:]
                 except IndexError as error:
                     pass
                 if mask is not None:
                     v = np.ma.masked_array(v)
-                    v.mask = mask
+                    v.mask = mask.nxdata
                 if self.maximum < v.max():
                     self.maximum = v.max()
             self.progress_bar.setVisible(False)
-        self.output.setText('Maximum Value: %s' % self.maximum)
+        self.output_maximum(self.maximum)
 
     def accept(self):
-        self.node.maximum = self.maximum
+        self.entry['data'].attrs['maximum'] = self.maximum
         super(MaximumDialog, self).accept()
