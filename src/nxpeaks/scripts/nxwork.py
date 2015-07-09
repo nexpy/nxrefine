e331e8a5a1ce485b8425bf11e9be5c30728d5807
Ray Osborn <rayosborn@mac.com>
2015-07-08 23:24:24 -0500

    Add nxorient to nxwork
    Only runs when the parent has been defined.

--- src/nxpeaks/scripts/nxwork.py
+++ src/nxpeaks/scripts/nxwork.py
@@ -78,6 +78,9 @@ def main():
         print "\n\nCopying parameters from %s\n" % parent
         subprocess.call('nxcopy -f %s -o %s' 
                         % (parent, wrapper_file), shell=True)
+        subprocess.call('nxorient -f %s -e f1' 
+                        % wrapper_file, shell=True)
+        
 
 if __name__=="__main__":
     main()
