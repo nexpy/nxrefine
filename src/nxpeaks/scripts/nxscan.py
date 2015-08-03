6d9a4ae5848585935061244c011ef559216d7d0a
Ray Osborn <rayosborn@mac.com>
2015-08-03 16:17:43 +0200

    Allow multiple detector positions in nxscan
    Before, it defaulted to ‘f1’, which is the preferred situation if the
    workflow is too slow to complete all of them. However, more generally,
    we want to complete all of them.

--- src/nxpeaks/scripts/nxscan.py
+++ src/nxpeaks/scripts/nxscan.py
@@ -1,11 +1,22 @@
 import argparse, os, subprocess, time
 
+
+def crash(msg):
+    print msg
+    exit(1)
+
+
 def main():
 
     parser = argparse.ArgumentParser(
         description="Make NeXus file and directories for new scan")
     parser.add_argument('-d', '--directory', help='sample directory')
     parser.add_argument('-p', '--parent', help='scan parent')
+    parser.add_argument('-f', '--filenames', default=['f1', 'f2', 'f3'], 
+        nargs='+', help='names of NeXus files to be linked to this file')
+    parser.add_argument('-m', '--maskfiles', 
+        default=['mask_f1', 'mask_f2', 'mask_f3'], nargs='+',
+        help='name of the pixel mask files')
     parser.add_argument('-t', '--temperatures', nargs='+', help='list of temperatures')
     
     args = parser.parse_args()
@@ -14,16 +25,24 @@ def main():
     parent = args.parent
     sample = os.path.basename(os.path.dirname(directory))
     label = os.path.basename(directory)
+    files = args.filenames
+    maskfiles = args.maskfiles
+    if len(maskfiles) < len(files):
+        if len(maskfiles) == 1:
+            maskfiles = [maskfiles] * len(files)
+        else:
+            crash('No. of maskfiles must same as no. of filenames or 1')
     temperatures = args.temperatures
 
     for temperature in temperatures:
         dir = temperature+'K'
         scan_dir = os.path.join(directory, dir)
-        while not os.path.exists(os.path.join(scan_dir, 'f1', 'done.txt')):
-            time.sleep(10)
-        subprocess.call('nxwork -s %s -l %s -d %s -t %s -f f1 -m mask_f1 -p %s'
-                        % (sample, label, dir, temperature, parent), shell=True)
-        subprocess.call('nxtransform -d %s  -f f1 -p %s' % (scan_dir, parent), shell=True)
+        for (f, m) in zip(files, maskfiles):
+            while not os.path.exists(os.path.join(scan_dir, 'f', 'done.txt')):
+                time.sleep(10)
+            subprocess.call('nxwork -s %s -l %s -d %s -t %s -f %s -m %s -p %s'
+                        % (sample, label, dir, temperature, f, m, parent), shell=True)
+        subprocess.call('nxtransform -d %s  -f %s -p %s' % (scan_dir, f, parent), shell=True)
 
 if __name__=="__main__":
     main()
