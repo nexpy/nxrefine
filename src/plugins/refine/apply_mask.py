5d269876d0985238c5516054b79e5f40789ec568
Ray Osborn <rayosborn@mac.com>
2015-10-08 13:47:07 -0400

    Update to use GridParameters

--- src/plugins/refine/apply_mask.py
+++ src/plugins/refine/apply_mask.py
@@ -1,4 +1,3 @@
-from nexpy.gui.pyqt import QtGui
 import numpy as np
 from nexpy.gui.datadialogs import BaseDialog
 from nexpy.gui.mainwindow import report_error
@@ -16,31 +15,17 @@ class MaskDialog(BaseDialog):
 
     def __init__(self, parent=None):
         super(MaskDialog, self).__init__(parent)
-        node = self.get_node()
-        self.root = node.nxroot
-        self.entry = node.nxentry
-        layout = QtGui.QVBoxLayout()
-        grid = QtGui.QGridLayout()
-        grid.setSpacing(10)
-        self.mask_box = QtGui.QLineEdit()
-        self.mask_box.setMinimumWidth(300)
-        grid.addWidget(QtGui.QLabel('Mask Path:'), 0, 0)
-        grid.addWidget(self.mask_box, 0, 1)
-        layout.addLayout(grid)
-        save_button = QtGui.QPushButton('Save Mask')
-        save_button.clicked.connect(self.save_mask)
-        layout.addWidget(save_button)
-        layout.addWidget(self.close_buttons())
-        self.setLayout(layout)
-        self.setWindowTitle('Mask Data')
-        
-        self.mask_box.setText('pilatus_mask/entry/mask')
-
-    def get_mask_path(self):
-        return self.mask_box.text()
+        self.select_entry(self.choose_entry)
+        self.parameters = GridParameters()
+        self.parameters.add('mask', 'pilatus_mask/entry/mask', 'Mask Path')
+        self.action_buttons(('Save Mask', self.save_mask))
+        self.set_layout(self.entry_layout, self.parameters.grid(), 
+                        self.action_buttons(('Save Mask', self.save_mask)), 
+                        self.close_buttons())
+        self.set_title('Mask Data')
 
     def save_mask(self):
-        mask = self.treeview.tree[self.get_mask_path()]
+        mask = self.treeview.tree[self.parameters['mask'].value]
         if mask.dtype != np.bool:
             raise NeXusError('Mask must be a Boolean array')
         elif len(mask.shape) == 1:
