c4b3dbb7dcbbb4652ecfc501a1bfc7ceb7df4355
Ray Osborn <rayosborn@mac.com>
2015-10-09 12:56:32 -0400

    Fix existence checks

--- src/nxpeaks/scripts/nxlink.py
+++ src/nxpeaks/scripts/nxlink.py
@@ -29,9 +29,9 @@ def link_files(nexus_file, scan_dir, filenames, maskfiles):
 
 def make_data(entry, scan_file, mask=None):
     root = nxload(scan_file)
-    if filename in root.entry:
+    if 'filename' in root.entry:
         entry.filename = root.entry.filename
-    if start_time in root.entry.start_time:
+    if 'start_time' in root.entry.start_time:
         entry.start_time = root.entry.start_time
     if 'instrument' not in entry:
         entry.instrument = root.entry.instrument
