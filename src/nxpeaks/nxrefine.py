c96c57dd62067badc256abdfe289b37a54691691
Ray Osborn <rayosborn@mac.com>
2017-03-21 16:48:29 -0500

    Remove redundant line

--- src/nxpeaks/nxrefine.py
+++ src/nxpeaks/nxrefine.py
@@ -264,6 +264,7 @@ class NXRefine(object):
         lines.append('parameters.twoThetaCorrection = 0;')
         lines.append('parameters.twoThetaNom = %s;' % (self.twotheta*radians))
         lines.append('parameters.omegaCorrection = 0;')
+        if self.omega_step 
         lines.append('parameters.omegaStep = %s;' % (self.omega_step*radians))
         lines.append('parameters.chiCorrection = 0;')
         lines.append('parameters.chiNom = %s;' % (self.chi*radians))
