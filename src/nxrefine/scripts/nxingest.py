a4ba350b9ae4246021ed3c110e477199a5842ab6
Ray Osborn <rayosborn@mac.com>
2018-04-06 16:37:00 -0500

    Change metadata file names

--- src/nxrefine/scripts/nxingest.py
+++ src/nxrefine/scripts/nxingest.py
@@ -4,8 +4,8 @@ import numpy as np
 from nexusformat.nexus import *
 
 
-def read_header(header_file, metadata_file, logs):
-    with open(header_file) as f:
+def read_metadata(head_file, meta_file, logs):
+    with open(head_file) as f:
         lines = f.readlines()
     for line in lines:
         key, value = line.split(', ')
@@ -15,9 +15,9 @@ def read_header(header_file, metadata_file, logs):
         except:
             pass
         logs[key] = value
-    metadata_input = np.genfromtxt(metadata_file, delimiter=',', names=True)
-    for i, key in enumerate(metadata_input.dtype.names):
-        logs[key] = [array[0] for array in metadata_input]
+    meta_input = np.genfromtxt(meta_file, delimiter=',', names=True)
+    for i, key in enumerate(meta_input.dtype.names):
+        logs[key] = [array[0] for array in meta_input]
 
 
 def main():
@@ -41,13 +41,13 @@ def main():
 
     root = nxload(wrapper_file, 'rw')    
     for f in files:
-        header_file = os.path.join(directory, f+'_header.txt')
-        metadata_file = os.path.join(directory, f+'_metadata.txt')
+        head_file = os.path.join(directory, f+'_head.txt')
+        meta_file = os.path.join(directory, f+'_meta.txt')
         entry = root[f]
         if 'logs' in entry['instrument']:
             del entry['instrument/logs']
         entry['instrument/logs'] = NXcollection()
-        read_header(f+'_header.txt', f+'_metadata.txt', entry['instrument/logs']))              
+        read_metadata(head_file, meta_file, entry['instrument/logs']))              
                 
 
 if __name__=="__main__":
