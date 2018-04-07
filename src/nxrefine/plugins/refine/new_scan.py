f7b23d23ea4ac80f5db9deba87bcc50f4bc9d727
Ray Osborn <rayosborn@mac.com>
2018-04-07 17:46:36 -0500

    Initialize NXdata links and goniometer angles

--- src/nxrefine/plugins/refine/new_scan.py
+++ src/nxrefine/plugins/refine/new_scan.py
@@ -27,10 +27,12 @@ class ScanDialog(BaseDialog):
         self.entries = {}
 
         self.setup_sample()
+        self.setup_scan()
 
         self.set_layout(self.directorybox('Choose Data Directory'), 
                         self.filebox('Choose Experiment File'),
                         self.sample.grid(header=False),
+                        self.scan.grid(header=False),
                         self.action_buttons(('Make Scan File', self.make_scan)),
                         self.close_buttons(close=True))
         self.set_title('New Scan')
@@ -41,6 +43,22 @@ class ScanDialog(BaseDialog):
         self.sample.add('label', 'label', 'Sample Label')
         self.sample.add('scan', 'scan', 'Scan Label')
         self.sample.add('temperature', 300.0, 'Temperature (K)')
+
+    def setup_entry(self, position):
+        entry = NXentry()
+        self.detectors[position] = GridParameters()
+        self.detectors[position].add('x', 0.0, 'Translation - x (mm)')
+        self.detectors[position].add('y', 0.0, 'Translation - y (mm)')
+        self.experiment_file['f%s' % position] = entry
+
+    def setup_scan(self):
+        self.scan = GridParameters()
+        self.scan.add('phi_start', -5.0, 'Phi Start')
+        self.scan.add('phi_end', 360.0, 'Phi End')
+        self.scan.add('phi_step', 0.1, 'Phi Step')
+        self.scan.add('chi', -90.0, 'Chi')
+        self.scan.add('omega', 0.0, 'Omega')
+        self.scan.add('frame_rate', 10.0, 'Frame Rate (Hz)')
 
     def copy_experiment(self, experiment):
         self.scan_file = NXroot()
@@ -58,6 +76,27 @@ class ScanDialog(BaseDialog):
                 entry['sample/temperature'].attrs['units'] = 'K'
             else:
                 entry.makelink(self.scan_file['entry/sample'])
+                phi_start = self.scan['phi_start'].value
+                phi_end = self.scan['phi_end'].value
+                phi_step = self.scan['phi_step'].value
+                chi = self.scan['chi'].value
+                omega = self.scan['omega'].value
+                frame_rate = self.scan['frame_rate'].value
+                if 'goniometer' not in entry['instrument']:
+                    entry['instrument/goniometer'] = NXgoniometer()
+                entry['instrument/goniometer/phi'] = [phi_start, phi_start+phi_step]
+                entry['instrument/goniometer/chi'] = chi
+                entry['instrument/goniometer/omega'] = omega
+                entry['data'] = NXdata()
+                scan = self.sample['scan'].value
+                entry['data'].nxsignal = NXlink('/entry/data/data', 
+                    file=os.path.join(scan, entry.nxname+'.nxs'))
+                entry['data/x_pixel'] = np.arange(1475, dtype=np.int32)
+                entry['data/y_pixel'] = np.arange(1679, dtype=np.int32)
+                entry['data/frame_number'] = np.arange(3649, dtype=np.int32)
+                entry['data'].nxaxes = [entry['data/frame_number'],
+                                        entry['data/y_pixel'],
+                                        entry['data/x_pixel']]
 
     def make_scan(self):
         home_directory = self.get_directory()
