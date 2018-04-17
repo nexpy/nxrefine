67fbbe7995d01a97e686b353bb3d47ed35bddebb
Ray Osborn <rayosborn@mac.com>
2018-04-17 10:18:26 -0500

    Updated for Sector 6 configuration

--- src/nxrefine/scripts/nxwork.py
+++ src/nxrefine/scripts/nxwork.py
@@ -12,13 +12,8 @@ def main():
     parser.add_argument('-s', '--sample', help='sample name')
     parser.add_argument('-l', '--label', help='sample label')
     parser.add_argument('-d', '--directory', default='', help='scan directory')
-    parser.add_argument('-e', '--extension', default='tiff', help='image extension')
-    parser.add_argument('-t', '--temperature', help='temperature of scan')
     parser.add_argument('-f', '--filenames', default=['f1', 'f2', 'f3'], 
-        nargs='+', help='names of NeXus files to be linked to this file')
-    parser.add_argument('-m', '--maskfiles', 
-        default=['mask_f1', 'mask_f2', 'mask_f3'], nargs='+',
-        help='name of the pixel mask files')
+        nargs='+', help='names of NeXus files linked to this file')
     parser.add_argument('-p', '--parent', help='file name of file to copy from')
     
     args = parser.parse_args()
@@ -38,12 +33,6 @@ def main():
     
     temperature = np.float32(args.temperature)
     files = args.filenames
-    maskfiles = args.maskfiles
-    if len(maskfiles) < len(files):
-        if len(maskfiles) == 1:
-            maskfiles = [maskfiles] * len(files)
-        else:
-            crash('No. of maskfiles must same as no. of filenames or 1')
     parent = args.parent
 
     label_path = '%s/%s' % (sample, label)
@@ -54,25 +43,15 @@ def main():
     if not os.path.exists(label_path):
         crash("Label does not exist: "+label_path)
     
-    if not os.path.exists(wrapper_file):
-        print()"\n\nSetting up %s\n" % wrapper_file)
-        subprocess.call('nxsetup -s %s -l %s -d %s -t %s -f %s'
-                        % (sample, label, directory, temperature, 
-                           ' '.join(files)), shell=True)
-
-    for (f, m) in zip(files, maskfiles):
-        path = '%s/%s/%s/%s' % (sample, label, directory, f)
-        if not os.path.exists(path+'.nxs'):
-            print("\n\nStacking %s.nxs\n" % path)
-            subprocess.call('nxstack -d %s -p scan -e %s -o %s.nxs -s scan.spec -c lzf'
-                            % (path, ext, path), shell=True)
-        print("\n\nLinking %s.nxs\n" % path)
-        subprocess.call('nxlink -s %s -l %s -d %s -f %s -m %s'
-                        % (sample, label, directory, f, m), shell=True)
-        print("\n\nDetermining maximum counts in %s.nxs\n" % path)
+    for f in files:
+        path = os.path.join(sample, label, directory)
+        print("\n\nProcessing %s" % path)
+        print("\n\nReading in metadata in %s\n" % f)
+        subprocess.call('nxingest -d %s -f %s' % (path, f), shell=True)
+        print("\n\nDetermining maximum counts in %s\n" % f)
         subprocess.call('nxmax -f %s -p %s/data'
                         % (wrapper_file, f), shell=True)
-        print("\n\nFinding peaks in %s.nxs\n" % path)
+        print("\n\nFinding peaks in %s\n" % f)
         subprocess.call('nxfind -f %s -p %s/data -s 500 -e 2500'
                         % (wrapper_file, f), shell=True)
 
