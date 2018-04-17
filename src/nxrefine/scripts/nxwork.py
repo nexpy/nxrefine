b5e97b2419c9b1de200a7b2a69132b10d2586a5e
Ray Osborn <rayosborn@mac.com>
2018-04-17 10:35:41 -0500

    Update nxwork for Sector 6

--- src/nxrefine/scripts/nxwork.py
+++ src/nxrefine/scripts/nxwork.py
@@ -9,8 +9,6 @@ def main():
 
     parser = argparse.ArgumentParser(
         description="Perform workflow for scan")
-    parser.add_argument('-s', '--sample', help='sample name')
-    parser.add_argument('-l', '--label', help='sample label')
     parser.add_argument('-d', '--directory', default='', help='scan directory')
     parser.add_argument('-f', '--filenames', default=['f1', 'f2', 'f3'], 
         nargs='+', help='names of NeXus files linked to this file')
@@ -22,44 +20,47 @@ def main():
     label = args.label
     directory = args.directory.rstrip('/')
     if sample is None and label is None:
-        sample = os.path.basename(os.path.dirname(os.path.dirname(directory)))   
-        label = os.path.basename(os.path.dirname(directory))
-        directory = os.path.basename(directory)
+        sample = os.path.basename(os.path.dirname(directory))  
+        label = os.path.basename(directory)
 
-    print("Processing sample '%s', label '%s', scan '%s'\n" % (sample,
-                                                               label,
-                                                               directory))
-    ext = args.extension
+    print("Processing sample '%s', label '%s'\n" % (sample, label))
     
     temperature = np.float32(args.temperature)
     files = args.filenames
     parent = args.parent
 
     label_path = '%s/%s' % (sample, label)
-    wrapper_file = '%s/%s_%s.nxs' % (label_path, sample, directory)
 
     if sample == None:
         crash('Requires sample!')
     if not os.path.exists(label_path):
         crash("Label does not exist: "+label_path)
-    
-    for f in files:
-        path = os.path.join(sample, label, directory)
-        print("\n\nProcessing %s" % path)
-        print("\n\nReading in metadata in %s\n" % f)
-        subprocess.call('nxingest -d %s -f %s' % (path, f), shell=True)
-        print("\n\nDetermining maximum counts in %s\n" % f)
-        subprocess.call('nxmax -f %s -p %s/data'
-                        % (wrapper_file, f), shell=True)
-        print("\n\nFinding peaks in %s\n" % f)
-        subprocess.call('nxfind -f %s -p %s/data -s 500 -e 2500'
-                        % (wrapper_file, f), shell=True)
 
-    if parent:
-        print("\n\nCopying parameters from %s\n" % parent)
-        subprocess.call('nxcopy -f %s -o %s' 
-                        % (parent, wrapper_file), shell=True)
-        
+    wrapper_files = [filename for filename in os.listdir(label_path) 
+                     if filename.endswith('.nxs')]
+
+    for wrapper_file in wrapper_files:
+        root = nxload(wrapper_file)    
+        for f in files:
+            path = os.path.join(sample, label, directory)
+            print("\n\nProcessing %s" % path)
+            if 'logs' not in root[f]['instrument']:
+                print("\n\nReading in metadata in %s\n" % f)
+                subprocess.call('nxingest -d %s -f %s' % (path, f), shell=True)
+            if 'maximum' not in root[f]['data'].attrs:
+                print("\n\nDetermining maximum counts in %s\n" % f)
+                subprocess.call('nxmax -f %s -p %s/data'
+                                % (wrapper_file, f), shell=True)
+            if 'peaks' not in root[f]:
+                print("\n\nFinding peaks in %s\n" % f)
+                subprocess.call('nxfind -f %s -p %s/data -s 500 -e 2500'
+                                % (wrapper_file, f), shell=True)
+
+        if parent:
+            print("\n\nCopying parameters from %s\n" % parent)
+            subprocess.call('nxcopy -f %s -o %s' 
+                            % (parent, wrapper_file), shell=True)
+    
 
 if __name__=="__main__":
     main()
