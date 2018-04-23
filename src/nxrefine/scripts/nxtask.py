3132fc74f0199932f93d26bff6f41ca3f6262da9
Ray Osborn <rayosborn@mac.com>
2018-04-23 15:11:27 -0500

    Add a dry-run option

--- src/nxrefine/scripts/nxtask.py
+++ src/nxrefine/scripts/nxtask.py
@@ -20,6 +20,7 @@ def main():
     parser.add_argument('-p', '--parent', help='file name of file to copy from')
     parser.add_argument('-r', '--refine', action='store_true',
                         help='refine lattice parameters')
+    parser.add_argument('-n', '--norun', action='store_true', help='dry run')
    
     args = parser.parse_args()
 
@@ -35,6 +36,7 @@ def main():
     first = args.first
     last = args.last
     refine = args.refine
+    dry_run = args.norun
 
     print("Processing %s" % wrapper_file)
     root = nxload(wrapper_file)
@@ -42,30 +44,36 @@ def main():
         print("Processing %s" % e)
         if 'logs' not in root[e]['instrument']:
             print("Reading in metadata in %s" % e)
-            subprocess.call('nxingest -d %s -e %s' % (directory, e), shell=True)
+            if not dry_run:
+                subprocess.call('nxingest -d %s -e %s' % (directory, e), shell=True)
         if 'maximum' not in root[e]['data'].attrs and not threshold:
             print("Determining maximum counts in %s" % f)
-            subprocess.call('nxmax -d %s -e %s'
-                            % (directory, e), shell=True)
+            if not dry_run:
+                subprocess.call('nxmax -d %s -e %s'
+                                % (directory, e), shell=True)
         if 'peaks' not in root[e]:
             print("Finding peaks in %s" % e)
-            if threshold:
-                subprocess.call('nxfind -d %s -e %s -t %s -f %s -l %s'
-                    % (directory, e, threshold, first, last), shell=True)
-            else:
-                subprocess.call('nxfind -d %s -e %s -f %s -l %s'
-                                % (directory, e, first, last), shell=True)
+            if not dry_run:
+                if threshold:
+                    subprocess.call('nxfind -d %s -e %s -t %s -f %s -l %s'
+                        % (directory, e, threshold, first, last), shell=True)
+                else:
+                    subprocess.call('nxfind -d %s -e %s -f %s -l %s'
+                                    % (directory, e, first, last), shell=True)
 
     if parent:
         print("Copying parameters from %s" % parent)
-        subprocess.call('nxcopy -i %s -o %s' 
-                        % (parent, wrapper_file), shell=True)
+        if not dry_run:
+            subprocess.call('nxcopy -i %s -o %s' 
+                            % (parent, wrapper_file), shell=True)
     if refine:
         print("Refining %s" % wrapper_file)
-        subprocess.call('nxrefine -d %s' % directory, shell=True)
-    if parent:
-        subprocess.call('nxtransform -d %s -p %s' % (directory, parent), shell=True)
-        subprocess.call('nxcombine -d %s' % directory, shell=True)
+        if not dry_run:
+            subprocess.call('nxrefine -d %s' % directory, shell=True)
+    if parent and 'transform.nxs' not in os.listdir(directory):
+        if not dry_run:
+            subprocess.call('nxtransform -d %s -p %s' % (directory, parent), shell=True)
+            subprocess.call('nxcombine -d %s' % directory, shell=True)
 
 
 if __name__=="__main__":
