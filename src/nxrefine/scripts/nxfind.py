e749531dd6385746809b0d1a4613d46c970db136
Ray Osborn <rayosborn@mac.com>
2018-05-05 12:40:46 -0500

    Add new threshold to the help text

--- src/nxrefine/scripts/nxfind.py
+++ src/nxrefine/scripts/nxfind.py
@@ -251,7 +251,7 @@ def main():
     parser.add_argument('-e', '--entries', default=['f1', 'f2', 'f3'], 
         nargs='+', help='names of entries to be searched')
     parser.add_argument('-t', '--threshold', type=float,
-                        help='peak threshold - defaults to maximum counts/20')
+                        help='peak threshold - defaults to maximum counts/10')
     parser.add_argument('-f', '--first', type=int, help='first frame')
     parser.add_argument('-l', '--last', type=int, help='last frame')
     parser.add_argument('-o', '--overwrite', action='store_true', 
