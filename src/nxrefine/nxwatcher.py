211708fe42ef112168b63b9973e7fe8b9140517f
Ray Osborn <rayosborn@mac.com>
2018-05-19 13:36:27 -0500

    Add new watcher daemon
    This uses the watchdog module to monitor changes to the file system. It will be used to launch data reduction once a data file has been transferred to a directory.

---
+++ src/nxrefine/nxwatcher.py
@@ -0,0 +1,56 @@
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+from .daemon import NXDaemon
+from .nxserver import NXServer
+from .nxreduce import NXReduce
+
+
+class NXWatcher(NXDaemon):
+
+    def __init__(self, directory):
+        self.directory = directory
+        self.task_directory = os.path.join(self.directory, 'tasks')
+        if 'tasks' not in os.listdir(self.directory):
+            os.mkdir(self.task_directory)
+        self.server = NXServer(self.directory)
+        self.watch_files = {}
+        self.pid_file = os.path.join(self.task_directory, 'nxwatcher.pid')
+        self.observer = Observer()
+        super(NXWatcher, self).__init__(self.pid_file)
+
+    def run(self):
+        event_handler = NXHandler(self.server)
+        self.observer.schedule(event_handler, self.directory, recursive=True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(10)
+        except:
+            self.observer.stop()
+        self.observer.join()
+
+
+class NXHandler(FileSystemEventHandler):
+
+    def __init__(self, server):
+        self.server = server
+        super(NXHandler, self).__init__()
+
+    @staticmethod
+    def on_any_event(event):
+        if event.is_directory:
+            return None
+        elif event.event_type == 'created':
+            if event.src_path.endswith('.h5'):
+                self.watch_files[event.src_path] = time.time()
+        elif event.event_type == 'modified':
+            if event.src_path in self.watch_files:
+                now = time.time()
+                if now - self.watch_files[event.src_path] > 120.0:
+                    reduce = NXReduce(os.path.dirname(event.src_path), entry)
+                    self.server.add_task('nxtask ')
+                    del self.watch_files[event.src_path]
+                else:
+                    self.watch_files[event.src_path] = time.time()
---
+++ src/nxrefine/scripts/nxwatcher.py
@@ -0,0 +1,35 @@
+import argparse
+import os
+import sys
+from nxrefine.nxwatcher import NXWatcher
+
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        description="Launch watcher to monitor new files")
+    parser.add_argument('-c', '--cwd', default='/data/user6idd', 
+                        help='directory containing GUP directories')
+    parser.add_argument('-g', '--gup', default='GUP-58981',
+                        help='GUP number, e.g., GUP-58981')
+    parser.add_argument('command', action='store',
+                         help='valid commands are: start|stop|restart')
+    
+    args = parser.parse_args()
+
+    server = NXWatcher(os.path.join(args.cwd, args.gup))
+    
+    if args.command == 'start':
+        server.start()
+    elif args.command == 'stop':
+        server.stop()
+    elif args.command == 'restart':
+        server.restart()
+    else:
+        print("Unknown command")
+        sys.exit(2)
+    sys.exit(0)
+
+
+if __name__=="__main__":
+    main()
