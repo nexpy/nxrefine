b313ec1179d99c79c21fbc9b48a367332df2f727
Ray Osborn <rayosborn@mac.com>
2018-05-19 13:38:11 -0500

    Turn server into a daemon
    This will distribute tasks entered in a queue to processes on differenc notde.

---
+++ src/nxrefine/nxserver.py
@@ -0,0 +1,109 @@
+import os
+import time
+from multiprocessing import Process, Queue, JoinableQueue
+from .daemon import NXDaemon
+
+
+class NXWorker(Process):
+    """Class for processing tasks on a specific node."""
+    def __init__(self, node, task_queue, result_queue, log_file):
+        Process.__init__(self)
+        self.node = node
+        self.task_queue = task_queue
+        self.result_queue = result_queue
+        self.log_file = log_file
+
+    def run(self):
+        while True:
+            next_task = self.task_queue.get()
+            if next_task is None:
+                self.log('%s: Exiting' % self.node)
+                self.task_queue.task_done()
+                break
+            else:
+                self.log("%s: Executing '%s'" % (self.node, next_task.command))
+                next_task.execute(self.node)
+            self.task_queue.task_done()
+            self.result_queue.put(next_task.command)
+        return
+
+    def log(self, message):
+        with open(self.log_file, 'a') as f:
+            f.write(message+'\n')
+
+
+class NXTask(object):
+    """Class for submitting tasks to different nodes."""
+    def __init__(self, path, command):
+        self.path = path
+        self.command = command
+
+    def execute(self, node):
+        subprocess.call("pdsh -w %s 'cd %s; %s >> tasks/%s.out'" 
+                        % (node, self.path, self.command, node), shell=True)
+
+
+class NXServer(NXDaemon):
+
+    def __init__(self, directory, node_file=None):
+        self.directory = directory
+        self.task_directory = os.path.join(directory, 'tasks')
+        if 'tasks' not in os.listdir(directory):
+            os.mkdir(self.task_directory)
+        self.task_list = os.path.join(self.task_directory, 'task_list')
+        if not os.path.exists(self.task_list):
+            os.mkfifo(self.task_list)  
+        if node_file is None:
+            self.node_file = os.path.join(self.task_directory, 'nodefile')
+        else:
+            self.node_file = node_file
+        self.nodes = self.read_nodes(self.node_file)
+        self.log_file = os.path.join(self.task_directory, 'nxserver.log')
+        self.pid_file = os.path.join(self.task_directory, 'nxserver.pid')
+
+        self.tasks = None
+        self.results = None
+        self.workers = []
+        
+        super(NXServer, self).__init__(self.pid_file)
+        
+    def read_nodes(self, node_file):
+        """Read available nodes"""
+        with open(node_file) as f:
+            nodes = [line.strip() for line in f.readlines()]
+        return nodes
+
+    def log(self, message):
+        with open(self.log_file, 'a') as f:
+            f.write(str(message)+'\n')
+
+    def run(self):
+        self.log('Starting server')
+        self.tasks = JoinableQueue()
+        self.results = Queue()    
+        self.workers = [NXWorker(node, self.tasks, self.results, self.log_file) 
+                        for node in self.nodes]
+        for worker in self.workers:
+            worker.start()
+        task_fifo = open(self.task_list, 'r')
+        while True:
+            time.sleep(5)
+            command = task_fifo.readline()[:-1]
+            if command == 'stop':
+                break
+            elif command:
+                self.tasks.put(NXTask(self.directory, command))
+        for worker in self.workers:
+            self.tasks.put(None)
+        self.tasks.join()
+        self.log("Stopping server")
+        super(NXServer, self).stop()
+
+    def stop(self):
+        self.add_task('stop')
+    
+    def add_task(self, command):
+        """Add a task to the server queue"""
+        task_fifo = os.open(self.task_list, os.O_RDWR)
+        os.write(task_fifo, (command+'\n').encode())
+    
--- src/nxrefine/scripts/nxserver.py
+++ src/nxrefine/scripts/nxserver.py
@@ -1,93 +1,35 @@
 import argparse
-import logging
 import os
-import time
-from multiprocessing import Process, Queue, JoinableQueue
-
-class Worker(Process):
-    
-    def __init__(self, node, task_queue, result_queue):
-        Process.__init__(self)
-        self.node = node
-        self.task_queue = task_queue
-        self.result_queue = result_queue
-
-    def run(self):
-        while True:
-            next_task = self.task_queue.get()
-            next_task.execute(self.node)
-            self.task_queue.task_done()
-            self.result_queue.put(next_task.command)
-        return
-
-
-class Task(object):
-
-    def __init__(self, path, command):
-        self.path = path
-        self.command = command
-
-    def execute(self, node):
-        print("pdsh -w %s 'cd %s; %s >> tasks/%s.out'"
-              % (node, self.path, self.command, node))
-#        subprocess.call("pdsh -w %s 'cd %s; %s >> tasks/%s.out'" 
-#                        % (node, self.path, self.command, node), shell=True)
-
-
-def read_nodes(node_file):
-    """Read available nodes"""
-    with open('nodefile') as f:
-        nodes = [line.strip() for line in f.readlines()]
-    return nodes
-
-def initialize_logs(log_file):
-    """Initialize the nxserver logger."""
-    handler = logging.handlers.RotatingFileHandler(log_file, 
-                                                   maxBytes=50000,
-                                                   backupCount=5)
-    fmt = '%(asctime)s - %(levelname)s - %(message)s'
-    formatter = logging.Formatter(fmt, None)
-    handler.setFormatter(formatter)
-    logging.root.addHandler(handler)
+import sys
+from nxrefine.nxserver import NXServer
 
 
 def main():
 
     parser = argparse.ArgumentParser(
-        description="Perform workflow for scan")
+        description="Launch server for data reduction workflow")
     parser.add_argument('-c', '--cwd', default='/data/user6idd', 
                         help='directory containing GUP directories')
-    parser.add_argument('-g', '--gup', default='GUP-57342',
-                        help='GUP number, e.g., GUP-57342')
+    parser.add_argument('-g', '--gup', default='GUP-58981',
+                        help='GUP number, e.g., GUP-58981')
+    parser.add_argument('command', action='store',
+                         help='valid commands are: start|stop|restart')
     
     args = parser.parse_args()
-    cwd = args.cwd
-    gup = args.gup
 
-    path = os.path.join(cwd, gup)
-    if 'tasks' not in os.listdir(path):
-        os.mkdir(os.path.join(path, 'tasks'))
-    task_list = os.path.join(path, 'tasks', 'task_list')
-    if not os.path.exists(task_list):
-        os.mkfifo(task_list)  
-
-    nodes = read_nodes(os.path.join(cwd, 'nodefile'))
-    initialize_logs(os.path.join(path, 'tasks', 'nxserver.log'))
-
-    tasks = JoinableQueue()
-    results = Queue()
+    server = NXServer(os.path.join(args.cwd, args.gup))
     
-    workers = [Workers(node, tasks, results) for node in nodes]
-    for worker in workers:
-        worker.start()
+    if args.command == 'start':
+        server.start()
+    elif args.command == 'stop':
+        server.stop()
+    elif args.command == 'restart':
+        server.restart()
+    else:
+        print("Unknown command")
+        sys.exit(2)
+    sys.exit(0)
 
-    task_fifo = open(task_list, 'r')    
-    while True:
-        time.sleep(10)
-        command = task_fifo.readline()[:-1]
-        logging.info("'%s' added to task queue" % command)
-        tasks.put(Task(path, command))    
-    
 
 if __name__=="__main__":
     main()
