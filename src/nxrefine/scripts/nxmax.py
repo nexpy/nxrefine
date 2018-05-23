654a34d21ce72a173ff5659d2eb9f28c8a933c78
Ray Osborn <rayosborn@mac.com>
2018-05-23 11:21:03 -0500

    Update to use NXReduce class

--- src/nxrefine/scripts/nxmax.py
+++ src/nxrefine/scripts/nxmax.py
@@ -10,55 +10,7 @@ from __future__ import print_function
 import argparse, os, sys, timeit
 import numpy as np
 from nexusformat.nexus import *
-
-
-def update_progress(i):
-    s = 'Frame %d' % i
-    if i > 0:
-        s = '\r' + s
-    print(s, end='')
-
-
-def find_maximum(entry):
-
-    field = entry['data'].nxsignal
-    if not os.path.exists(field.nxfilename):
-        print("'%s' does not exist" % field.nxfilename)
-        return
-
-    tic=timeit.default_timer()
-
-    maximum = 0.0
-    try:
-        mask = entry['instrument/detector/pixel_mask'].nxdata
-        if len(mask.shape) > 2:
-            mask = mask[0]
-    except Exception:
-        mask = None
-    if len(field.shape) == 2:
-        maximum = field[:,:].max()
-    else:
-        nframes = field.shape[0]
-        chunk_size = field.chunks[0]
-        for i in range(0, nframes, chunk_size):
-            try:
-                update_progress(i)
-                v = field[i:i+chunk_size,:,:]
-            except IndexError as error:
-                pass
-            if mask is not None:
-                v = np.ma.masked_array(v)
-                v.mask = mask
-            if maximum < v.max():
-                maximum = v.max()
-            del v
-
-    entry['data'].attrs['maximum'] = maximum
-    
-    print('\nMaximum counts in %s are' % entry.nxname, maximum)
-
-    toc=timeit.default_timer()
-    print(toc-tic, 'seconds for', entry.nxname)
+from nxrefine.nxreduce import NXReduce
 
 
 def main():
@@ -74,28 +26,9 @@ def main():
 
     args = parser.parse_args()
 
-    directory = args.directory.rstrip('/')
-    sample = os.path.basename(os.path.dirname(os.path.dirname(directory)))   
-    label = os.path.basename(os.path.dirname(directory))
-    scan = os.path.basename(directory)
-    wrapper_file = os.path.join(sample, label, '%s_%s.nxs' % (sample, scan))
-    entries = args.entries
-    overwrite = args.overwrite
-
-    if not os.path.exists(wrapper_file):
-        print("'%s' does not exist" % wrapper_file)
-        sys.exit(1)
-    else:
-        root = nxload(wrapper_file, 'rw')
-
-    print('Finding maximum in ', wrapper_file)
-
-    for entry in entries:
-        print('Processing', entry)
-        if 'maximum' in root[entry]['data'].attrs and not overwrite:
-            print('Maximum value already determined')
-        else:
-            find_maximum(root[entry])
+    for entry in args.entries:
+        reduce = NXReduce(args.directory, entry, overwrite=args.overwrite)
+        reduce.nxmax()
 
 
 if __name__=="__main__":
