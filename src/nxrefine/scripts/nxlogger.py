d1bdfc68f206ea8aa7837548fe23114d10829392
Ray Osborn <rayosborn@mac.com>
2018-05-23 11:18:33 -0500

    Add NXLogger daemon

---
+++ src/nxrefine/scripts/nxlogger.py
@@ -0,0 +1,31 @@
+import argparse
+import os
+import sys
+from nxrefine.nxlogger import NXLogger
+
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        description="Launch logger for data reduction workflow")
+    parser.add_argument('-c', '--cwd', default='/data/user6idd', 
+                        help='directory containing GUP directories')
+    parser.add_argument('-g', '--gup', default='GUP-58981',
+                        help='GUP number, e.g., GUP-58981')
+    parser.add_argument('command', action='store',
+                         help='valid commands are: start|stop|restart')
+    
+    args = parser.parse_args()
+
+    logger = NXLogger(os.path.join(args.cwd, args.gup))
+    
+    if args.command == 'start':
+        logger.start()
+    elif args.command == 'stop':
+        logger.stop()
+    elif args.command == 'restart':
+        logger.restart()
+
+
+if __name__=="__main__":
+    main()
