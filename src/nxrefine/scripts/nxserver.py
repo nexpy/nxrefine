37240c31121b0a927753e79b0c76ef20a7648a4a
Ray Osborn <rayosborn@mac.com>
2018-05-23 11:19:01 -0500

    Remove redundant check
    This is already done by the argparser module.

--- src/nxrefine/scripts/nxserver.py
+++ src/nxrefine/scripts/nxserver.py
@@ -25,10 +25,6 @@ def main():
         server.stop()
     elif args.command == 'restart':
         server.restart()
-    else:
-        print("Unknown command")
-        sys.exit(2)
-    sys.exit(0)
 
 
 if __name__=="__main__":
