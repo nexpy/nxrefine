a90ad32c2349c2cfd5154901fb2e07bc48ae9bda
Ray Osborn <rayosborn@mac.com>
2018-06-02 14:50:46 -0500

    Handle malformed files
    Otherwise, the exception kills the process.

--- src/nxrefine/scripts/nxsummary.py
+++ src/nxrefine/scripts/nxsummary.py
@@ -29,18 +29,23 @@ def main():
         for e in args.entries:        
             print("Processing %s" % e)
             status = '%s[%s]:' % (wrapper_file, e)
-            if 'nxlink' in root[e] or 'logs' in root[e]['instrument']:
-                status = status + ' nxlink'
-            if 'nxmax' in root[e] or 'maximum' in root[e]['data'].attrs:
-                status = status + ' nxmax'
-            if 'nxfind' in root[e] or 'peaks' in root[e]:
-                status = status + ' nxfind'
-            if 'nxcopy' in root[e]:
-                status = status + ' nxcopy'
-            if 'nxrefine' in root[e] or 'orientation_matrix' in root[e]['instrument/detector']:
-                status = status + ' nxrefine'
-            if 'nxtransform' in root[e] or 'transform' in root[e]:
-                status = status + ' nxtransform'
+            if e in root and 'data' in root[e] and 'instrument' in root[e]:
+                if 'nxlink' in root[e] or 'logs' in root[e]['instrument']:
+                    status = status + ' nxlink'
+                if 'nxmax' in root[e] or 'maximum' in root[e]['data'].attrs:
+                    status = status + ' nxmax'
+                if 'nxfind' in root[e] or 'peaks' in root[e]:
+                    status = status + ' nxfind'
+                if 'nxcopy' in root[e]:
+                    status = status + ' nxcopy'
+                if ('nxrefine' in root[e] or 
+                    ('detector' in root[e] and 
+                     'orientation_matrix' in root[e]['instrument/detector'])):
+                    status = status + ' nxrefine'
+                if 'nxtransform' in root[e] or 'transform' in root[e]:
+                    status = status + ' nxtransform'
+            else:
+                status = status + ' file incomplete'
             summary.append(status)            
             
     summary_file = os.path.join(directory, 'nxsummary.log')
