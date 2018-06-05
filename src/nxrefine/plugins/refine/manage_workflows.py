0eede6a832053569b1dc0f07ea47a874965eb35d
Ray Osborn <rayosborn@mac.com>
2018-06-05 17:19:04 -0500

    Enhance workflow control

--- src/nxrefine/plugins/refine/manage_workflows.py
+++ src/nxrefine/plugins/refine/manage_workflows.py
@@ -2,10 +2,11 @@ import os
 
 from nexusformat.nexus import *
 from nexpy.gui.pyqt import QtCore, QtWidgets
-from nexpy.gui.datadialogs import BaseDialog
+from nexpy.gui.datadialogs import BaseDialog, GridParameters
 from nexpy.gui.utils import report_error, natural_sort
 
-from nxrefine.nxreduce import Lock
+from nxrefine.nxreduce import Lock, NXReduce
+from nxrefine.nxserver import NXServer
 
 
 def show_dialog():
@@ -20,25 +21,62 @@ class WorkflowDialog(BaseDialog):
     def __init__(self, parent=None):
         super(WorkflowDialog, self).__init__(parent)
 
-        self.set_layout(self.directorybox('Choose Sample Directory',
-                                          slot=self.choose_directory),
-                        self.close_buttons())
+        self.set_layout(self.directorybox('Choose Sample Directory'),
+                        self.filebox('Choose Parent File'),
+                        self.action_buttons(('Update Status', self.update),
+                                            ('Add to Queue', self.add_tasks)),
+                        self.close_buttons(close=True))
         self.set_title('Manage Workflows')
+        self.grid = None
         self.sample_directory = None
 
     def choose_directory(self):
         super(WorkflowDialog, self).choose_directory()
         self.sample_directory = self.get_directory()
         self.sample = os.path.basename(os.path.dirname(self.sample_directory))
+        parent_file = os.path.join(self.sample_directory, 
+                                   self.sample+'_parent.nxs')
+        if os.path.exists(parent_file):
+            self.filename.setText(os.path.basename(os.path.realpath(parent_file)))
+        else:
+            self.filename.setText('')
+        self.root_directory = os.path.dirname(os.path.dirname(self.sample_directory))
+        self.server = NXServer(self.root_directory)    
         self.mainwindow.default_directory = self.sample_directory
-        self.insert_layout(1, self.prepare_grid())
+        if self.grid:
+            self.delete_grid(self.grid)
+            self.grid = None
 
-    def prepare_grid(self):
+    def choose_file(self):
+        super(WorkflowDialog, self).choose_file()
+        self.make_parent()
+
+    def make_parent(self):
+        _parent = self.get_filename()
+        _base = os.path.basename(os.path.splitext(_parent)[0])
+        _scan = _base.replace(self.sample+'_', '')
+        reduce = NXReduce(directory=os.path.join(self.sample_directory, _scan), 
+                          overwrite=True)
+        reduce.make_parent()
+        if _parent:
+            return _parent
+        else:
+            return None
+
+    def is_parent(self, wrapper_file):
+        parent_file = os.path.join(self.sample_directory, 
+                                   self.sample+'_parent.nxs')
+        if os.path.exists(parent_file):
+            return wrapper_file == os.path.realpath(parent_file)
+        else:
+            return False
+
+    def update(self):
         grid = QtWidgets.QGridLayout()
         grid.setSpacing(1)
         row = 0
         columns = ['Scan', 'nxlink', 'nxmax', 'nxfind', 'nxcopy', 'nxrefine', 
-                   'nxtransform', 'nxcombine', 'overwrite']
+                   'nxtransform', 'nxcombine', 'overwrite', 'reduce']
         header = {}
         for col, column in enumerate(columns):
             header[column] = QtWidgets.QLabel(column)
@@ -48,15 +86,18 @@ class WorkflowDialog(BaseDialog):
         wrapper_files = sorted([os.path.join(self.sample_directory, filename) 
                             for filename in os.listdir(self.sample_directory) 
                             if filename.endswith('.nxs')], key=natural_sort)
-        self.scan = {}
+        self.scans = {}
         for wrapper_file in wrapper_files:
             row += 1
             base_name = os.path.basename(os.path.splitext(wrapper_file)[0])
-            directory = base_name.replace(self.sample+'_', '')
+            scan_label = base_name.replace(self.sample+'_', '')
+            if scan_label == 'parent':
+                break
+            directory = os.path.join(self.sample_directory, scan_label)
             status = {}
             with Lock(wrapper_file):
                 root = nxload(wrapper_file)
-            grid.addWidget(QtWidgets.QLabel(directory), row, 0, QtCore.Qt.AlignHCenter)
+            grid.addWidget(QtWidgets.QLabel(scan_label), row, 0, QtCore.Qt.AlignHCenter)
             status['nxlink'] = self.new_checkbox()
             status['nxmax'] = self.new_checkbox()
             status['nxfind'] = self.new_checkbox()
@@ -64,7 +105,8 @@ class WorkflowDialog(BaseDialog):
             status['nxrefine'] = self.new_checkbox()
             status['nxtransform'] = self.new_checkbox()
             status['nxcombine'] = self.new_checkbox()
-            status['overwrite'] = self.new_checkbox()
+            status['overwrite'] = self.new_checkbox() 
+            status['reduce'] = self.new_checkbox() 
             for i,e in enumerate([entry for entry in root.entries if entry != 'entry']):
                 if e in root and 'data' in root[e] and 'instrument' in root[e]:
                     self.update_checkbox(status['nxlink'], i,
@@ -73,7 +115,8 @@ class WorkflowDialog(BaseDialog):
                         'nxmax' in root[e] or 'maximum' in root[e]['data'].attrs)
                     self.update_checkbox(status['nxfind'], i,
                         'nxfind' in root[e] or 'peaks' in root[e])
-                    self.update_checkbox(status['nxcopy'], i, 'nxcopy' in root[e])
+                    self.update_checkbox(status['nxcopy'], i, 
+                        'nxcopy' in root[e] or self.is_parent(wrapper_file))
                     self.update_checkbox(status['nxrefine'], i,
                         'nxrefine' in root[e] or 
                         ('detector' in root[e]['instrument'] and 
@@ -87,6 +130,7 @@ class WorkflowDialog(BaseDialog):
                     status['nxcopy'].setEnabled(False)
                     status['nxrefine'].setEnabled(False)
                     status['nxtransform'].setEnabled(False)
+                    status['reduce'].setEnabled(False)
                     status['overwrite'].setEnabled(False)
             self.update_checkbox(status['nxcombine'], 0,
                 'nxcombine' in root['entry'] or 
@@ -100,18 +144,20 @@ class WorkflowDialog(BaseDialog):
             grid.addWidget(status['nxtransform'], row, 6, QtCore.Qt.AlignHCenter)                   
             grid.addWidget(status['nxcombine'], row, 7, QtCore.Qt.AlignHCenter)                   
             grid.addWidget(status['overwrite'], row, 8, QtCore.Qt.AlignHCenter)                  
-            self.scan[directory] = status
+            grid.addWidget(status['reduce'], row, 9, QtCore.Qt.AlignHCenter)                  
+            self.scans[directory] = status
         row += 1
         grid.addWidget(QtWidgets.QLabel('All'), row, 0, QtCore.Qt.AlignHCenter)
         all_boxes = {}
-        all_boxes['nxlink'] = self.new_checkbox(self.choose_all)
-        all_boxes['nxmax'] = self.new_checkbox(self.choose_all)
-        all_boxes['nxfind'] = self.new_checkbox(self.choose_all)
-        all_boxes['nxcopy'] = self.new_checkbox(self.choose_all)
-        all_boxes['nxrefine'] = self.new_checkbox(self.choose_all)
-        all_boxes['nxtransform'] = self.new_checkbox(self.choose_all)
-        all_boxes['nxcombine'] = self.new_checkbox(self.choose_all)
-        all_boxes['overwrite'] = self.new_checkbox(self.choose_all)        
+        all_boxes['nxlink'] = self.new_checkbox(self.choose_all_scans)
+        all_boxes['nxmax'] = self.new_checkbox(self.choose_all_scans)
+        all_boxes['nxfind'] = self.new_checkbox(self.choose_all_scans)
+        all_boxes['nxcopy'] = self.new_checkbox(self.choose_all_scans)
+        all_boxes['nxrefine'] = self.new_checkbox(self.choose_all_scans)
+        all_boxes['nxtransform'] = self.new_checkbox(self.choose_all_scans)
+        all_boxes['nxcombine'] = self.new_checkbox(self.choose_all_scans)
+        all_boxes['overwrite'] = self.new_checkbox(self.choose_all_scans)        
+        all_boxes['reduce'] = self.new_checkbox(self.choose_all_scans)        
         grid.addWidget(all_boxes['nxlink'], row, 1, QtCore.Qt.AlignHCenter)                   
         grid.addWidget(all_boxes['nxmax'], row, 2, QtCore.Qt.AlignHCenter)                   
         grid.addWidget(all_boxes['nxfind'], row, 3, QtCore.Qt.AlignHCenter)                   
@@ -120,7 +166,12 @@ class WorkflowDialog(BaseDialog):
         grid.addWidget(all_boxes['nxtransform'], row, 6, QtCore.Qt.AlignHCenter)                   
         grid.addWidget(all_boxes['nxcombine'], row, 7, QtCore.Qt.AlignHCenter)                   
         grid.addWidget(all_boxes['overwrite'], row, 8, QtCore.Qt.AlignHCenter)
-        self.select_all = all_boxes                
+        grid.addWidget(all_boxes['reduce'], row, 9, QtCore.Qt.AlignHCenter)
+        self.all_scans = all_boxes
+        if self.grid:
+            self.delete_grid(self.grid)
+        self.grid = grid
+        self.insert_layout(2, self.grid)           
         return grid
 
     def new_checkbox(self, slot=None):
@@ -140,17 +191,21 @@ class WorkflowDialog(BaseDialog):
             checkbox.setCheckState(QtCore.Qt.PartiallyChecked)
             checkbox.setEnabled(True)
 
-    def choose_all(self, key):
-        for status in self.select_all:
-            for scan in self.scan:
-                if self.scan[scan][status].isEnabled():
-                    self.scan[scan][status].setCheckState(
-                        self.select_all[status].checkState())                
+    def choose_all_scans(self):
+        for status in self.all_scans:
+            for scan in self.scans:
+                if self.scans[scan][status].isEnabled():
+                    self.scans[scan][status].setCheckState(
+                        self.all_scans[status].checkState())                
 
-    def accept(self):
-        if not self.sample_directory:
-            raise NeXusError('Sample directory not chosen')
-        try:
-            super(WorkflowDialog, self).accept()
-        except Exception as error:
-            report_error("Managing Workflows", error)
+    def add_tasks(self):
+        for scan in self.scans:
+            if self.scans[scan]['reduce'].isChecked():
+                for entry in ['f1', 'f2', 'f3']:
+                    reduce = NXReduce(entry, scan)
+                    if (self.scans[scan]['nxrefine'].isEnabled() and 
+                        self.scans[scan]['nxrefine'].isChecked()):
+                        reduce.refine = True
+                    if self.scans[scan]['overwrite'].isChecked():
+                        reduce.overwrite=True
+                    self.server.add_task(reduce.command)                            
