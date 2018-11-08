dd100e88ca7bba711cd5d3b3383f2ee458cd05c5
Ray Osborn <rayosborn@mac.com>
2018-11-08 12:07:08 -0600

    Update server dialog to use NXServer

--- src/nxrefine/plugins/refine/manage_servers.py
+++ src/nxrefine/plugins/refine/manage_servers.py
@@ -4,13 +4,15 @@ import subprocess
 from nexusformat.nexus import *
 from nexpy.gui.datadialogs import BaseDialog
 from nexpy.gui.utils import report_error
+from nxrefine.nxserver import NXServer
 
 
 def show_dialog():
-    dialog = ServerDialog()
-    dialog.show()
-#    except NeXusError as error:
-#        report_error("Defining New Experiment", error)
+    try:
+        dialog = ServerDialog()
+        dialog.show()
+    except NeXusError as error:
+        report_error("Managing Servers", error)
 
 
 class ServerDialog(BaseDialog):
@@ -20,54 +22,34 @@ class ServerDialog(BaseDialog):
 
         self.node_editor = self.editor()
         self.set_layout(self.directorybox('Choose Experiment Directory',
-                                          slot=self.choose_directory),
-                        self.checkboxes(('server', 'Server', False),
-                                        ('watcher', 'Watcher', False),
-                                        ('logger', 'Logger', False)),
+                                          slot=self.choose_directory,
+                                          default=False),
+                        self.action_buttons(('server', self.toggle_server)),
                         self.labels(('List of Nodes'), header=True),
                         self.node_editor,
-                        self.close_buttons())
+                        self.close_buttons(close=True))
+        self.pushbutton['server'].setText('Start/Stop Server')
         self.node_editor.setFocus()
         self.set_title('Manage Servers')
-        self.home_directory = None
+        self.experiment_directory = None
 
     def choose_directory(self):
         super(ServerDialog, self).choose_directory()
-        self.home_directory = self.get_directory()
-        self.mainwindow.default_directory = self.home_directory
-        task_directory = os.path.join(self.home_directory, 'tasks')
-        if not os.path.exists(task_directory):
-            os.makedirs(task_directory)
-        if os.path.exists(os.path.join(task_directory, 'nodefile')):
-            with open(os.path.join(task_directory, 'nodefile')) as f:
-                self.node_editor.setPlainText(f.read())
-        if os.path.exists(os.path.join(task_directory, 'nxserver.pid')):
-            self.checkbox['server'].setChecked(True)
-        if os.path.exists(os.path.join(task_directory, 'nxwatcher.pid')):
-            self.checkbox['watcher'].setChecked(True)
-        if os.path.exists(os.path.join(task_directory, 'nxlogger.pid')):
-            self.checkbox['logger'].setChecked(True)
+        self.experiment_directory = self.get_directory()
+        self.mainwindow.default_directory = self.experiment_directory
+        self.server = NXServer(self.experiment_directory)
+        self.node_editor.setPlainText('\n'.join(self.server.nodes))
+        if self.server.is_running():
+            self.pushbutton['server'].setText('Stop Server')
+        else:
+            self.pushbutton['server'].setText('Start Server')
 
-    def accept(self):
-        if not self.home_directory:
+    def toggle_server(self):
+        if not self.experiment_directory:
             raise NeXusError('Experiment directory not chosen')
-        try:
-            with open(os.path.join(self.home_directory, 'tasks', 'nodefile'), 'w') as f:
-                f.write(self.node_editor.toPlainText())
-            switches = '-c %s -g %s' % (os.path.dirname(self.home_directory),
-                                        os.path.basename(self.home_directory))
-            if self.checkbox['server'].isChecked():
-                subprocess.run('nxserver %s start' % switches, shell=True)
-            else:
-                subprocess.run('nxserver %s stop' % switches, shell=True)
-            if self.checkbox['watcher'].isChecked():
-                subprocess.run('nxwatcher %s start' % switches, shell=True)
-            else:
-                subprocess.run('nxwatcher %s stop' % switches, shell=True)
-            if self.checkbox['logger'].isChecked():
-                subprocess.run('nxlogger %s start' % switches, shell=True)
-            else:
-                subprocess.run('nxlogger %s stop' % switches, shell=True)
-            super(ServerDialog, self).accept()
-        except Exception as error:
-            report_error("Managing Servers", error)
+        elif self.pushbutton['server'].text() == 'Start Server':
+            self.server.start()
+            self.pushbutton['server'].setText('Stop Server')
+        else:
+            self.server.stop()
+            self.pushbutton['server'].setText('Start Server')
