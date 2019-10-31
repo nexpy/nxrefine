6ebd9b6150df652cd72f80e63dfbc73de148403f
Ray Osborn <rayosborn@mac.com>
2019-10-31 16:15:46 -0500

    Fix create_mask compatibility

--- src/nxrefine/plugins/refine/create_mask.py
+++ src/nxrefine/plugins/refine/create_mask.py
@@ -2,7 +2,8 @@ from __future__ import absolute_import
 
 import numpy as np
 from nexpy.gui.datadialogs import BaseDialog, GridParameters
-from nexpy.gui.plotview import NXPlotView, plotviews, NXrectangle, NXcircle
+from nexpy.gui.plotview import NXPlotView, plotviews
+from nexpy.gui.widgets import NXrectangle, NXcircle
 from nexpy.gui.utils import report_error
 from nexusformat.nexus import NeXusError
 
@@ -22,14 +23,17 @@ class MaskDialog(BaseDialog):
         
         self.plotview = None
         self.shapes = []
-        
+        self.parameters = {}
         self.select_entry(self.choose_entry)
         self.shape_box = self.select_box(['Rectangle', 'Circle'])
+        self.shape_choice = self.select_box([], slot=self.choose_shape)
         self.set_layout(self.entry_layout, 
                         self.make_layout(
                             self.action_buttons(('Add Shape', self.add_shape)),
                             self.shape_box),
+                        self.shape_choice,
                         self.close_buttons(save=True))
+        self.shape_choice.setVisible(False)
         self.set_title('Mask Data')
 
     def choose_entry(self):
@@ -38,6 +42,8 @@ class MaskDialog(BaseDialog):
         self.data = self.entry['instrument/calibration']
         self.counts = self.data.nxsignal.nxvalue
         self.mask = self.entry['instrument/detector/pixel_mask'].nxvalue
+        self.xc = self.entry['instrument/detector/beam_center_x'].nxvalue
+        self.yc = self.entry['instrument/detector/beam_center_y'].nxvalue
         self.plot_data()
         shape = self.data.nxsignal.shape
 
@@ -53,23 +59,43 @@ class MaskDialog(BaseDialog):
         self.plotview.draw()
 
     def add_shape(self):
-        self.plotview.deactivate()
-        xlo, xhi = self.plotview.xaxis.lo, self.plotview.xaxis.hi
-        ylo, yhi = self.plotview.yaxis.lo, self.plotview.yaxis.hi
-        xc, yc = xlo + 0.5 * (xhi - xlo), ylo + 0.5 * (yhi - ylo)
-        r = (xhi - xlo) / 4
         if self.shape_box.currentText() == 'Rectangle':
-            self.shapes.append(NXrectangle(xc-r, yc-r, 2*r, 2*r,
-                                           border_tol=0.1, plotview=self.plotview,
-                                           facecolor='r', edgecolor='k',
+            self.shapes.append(NXrectangle(self.xc-50, self.yc-50, 100, 100,
+                                           border_tol=0.1, 
+                                           plotview=self.plotview,
+                                           resize=True, facecolor='r', 
+                                           edgecolor='k',
                                            linewidth=1, alpha=0.3))
         else:
-            self.shapes.append(NXcircle(xc, yc, r, border_tol=0.1, 
-                                        plotview=self.plotview,
+            self.shapes.append(NXcircle(self.xc, self.yc, 50, border_tol=0.1, 
+                                        plotview=self.plotview, resize=True,
                                         facecolor='r', edgecolor='k',
                                         linewidth=1, alpha=0.3))
         self.plotview.draw()
-        self.shapes[-1].connect()    
+        self.shapes[-1].connect()
+        self.shape_choice.addItem(repr(self.shapes[-1]))
+        self.shape_choice.setVisible(True)
+        self.insert_layout(self.shape_options(self.shapes[-1]))
+
+    def shape_options(self, shape): 
+        p = self.parameters[shape] = GridParameters()
+        if isinstance(shape, NXrectangle):
+            x, y = shape.rectangle.xy
+            w, h = shape.rectangle.get_width(), self.rectangle.get_height()
+            p.add('x', x, 'Left Pixel')
+            p.add('y', y, 'Bottom Pixel')
+            p.add('w', w, 'Width')
+            p.add('h', h, 'Height')
+        else:
+            x, y = shape.circle.center
+            r = abs(shape.circle.width) / 2
+            p.add('x', x, 'X-Center')
+            p.add('y', y, 'Y-Center')
+            p.add('r', r, 'Radius')
+        return p.grid(header=False)
+
+    def choose_shape(self):
+        pass
 
     def accept(self):
         x, y = np.arange(self.mask.shape[1]), np.arange(self.mask.shape[0])
@@ -82,7 +108,7 @@ class MaskDialog(BaseDialog):
             else:
                 circle = shape.circle
                 xc, yc = circle.center
-                r = circle.radius
+                r = shape.radius
                 inside = (x[None,:]-int(xc))**2+(y[:,None]-int(yc))**2 < r**2
                 self.mask = self.mask | inside
         self.mask[np.where(self.counts<0)] = 1
