28ea3fe93ee1f5e540b6220c064015a3927fb89f
Ray Osborn <rayosborn@mac.com>
2021-01-26 16:18:36 -0600

    Add node editor when the server type is multinode

--- src/nxrefine/plugins/refine/manage_servers.py
+++ src/nxrefine/plugins/refine/manage_servers.py
@@ -6,6 +6,7 @@ from nexpy.gui.pyqt import QtWidgets, getOpenFileName
 from nexusformat.nexus import *
 from nexpy.gui.datadialogs import NXDialog
 from nexpy.gui.utils import report_error
+from nexpy.gui.widgets import NXPlainTextEdit
 from nxrefine.nxserver import NXServer
 
 
@@ -34,10 +35,18 @@ class ServerDialog(NXDialog):
             self.pushbutton['server'].setText('Start Server')
         self.server_layout = self.make_layout(self.server_status, 
                                               self.server_actions)
-
-        self.set_layout(self.labels(('Server Status'), header=True),
-                        self.server_layout,
-                        self.close_buttons(save=True))
+        if self.server_type == 'multinode':
+            self.node_editor = NXPlainTextEdit()
+            self.node_editor.setPlainText('\n'.join(self.server.read_nodes()))
+            self.set_layout(self.labels(('Server Status'), header=True),
+                            self.server_layout,
+                            self.labels(('List of Nodes'), header=True),
+                            self.node_editor,
+                            self.close_buttons(save=True))
+        else:
+            self.set_layout(self.labels(('Server Status'), header=True),
+                            self.server_layout,
+                            self.close_buttons(save=True))
 
         self.set_title('Manage Servers')
         self.experiment_directory = None
