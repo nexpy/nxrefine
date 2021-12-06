428b5224e9bf11a004c7a0880c15b021e00554a0
Ray Osborn <rayosborn@mac.com>
2021-12-06 16:28:44 -0600

    Update the long description

--- src/nxrefine/__init__.py
+++ src/nxrefine/__init__.py
@@ -1,30 +1,27 @@
-#-----------------------------------------------------------------------------
-# Copyright (c) 2013, NeXpy Development Team.
+# -----------------------------------------------------------------------------
+# Copyright (c) 2013-2021, NeXpy Development Team.
 #
 # Distributed under the terms of the Modified BSD License.
 #
 # The full license is in the file COPYING, distributed with this software.
-#-----------------------------------------------------------------------------
-from __future__ import absolute_import
+# -----------------------------------------------------------------------------
 
-__package_name__ = u'NXrefine'
+__package_name__ = 'NXrefine'
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
-__documentation_author__ = u'Ray Osborn'
-__documentation_copyright__ = u'2013-17, Ray Osborn'
+__documentation_author__ = 'Ray Osborn'
+__documentation_copyright__ = '2013-21, Ray Osborn'
 
-__license__ = u'BSD'
-__author_name__ = u'NeXpy Development Team'
-__author_email__ = u'nexpydev@gmail.com'
-__author__ = __author_name__ + u' <' + __author_email__ + u'>'
+__license__ = 'BSD'
+__author_name__ = 'Ray Osborn'
+__author_email__ = 'rosborn@anl.gov'
+__author__ = __author_name__ + ' <' + __author_email__ + '>'
 
-__description__ = u'NXrefine: Python package to process crystallographic data'
-__long_description__ = \
-u"""
-This is designed to be used in coordination with NeXpy. It utilizes software
-developed by Jon Wright as part of the ImageD11 package to search for Bragg
-peaks within multiple images. It has been adapted to work on three-dimensional
-NeXus files, rather than multiple TIFF files.
-"""
+__description__ = 'NXrefine: Python package to process crystallographic data'
+__long_description__ = ("""NXRefine implements a complete workflow for the
+reduction of single-crystal x-ray scattering measured on an area detector as a
+series of frames collected while rotating a sample continuously in a
+monochromatic beam.
+""")
