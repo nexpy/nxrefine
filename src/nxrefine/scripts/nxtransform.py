a53db25cbfb61e360465d55d6bcdb9b16e54a9ad
Ray Osborn <rayosborn@mac.com>
2022-01-20 18:01:14 -0600

    Remove width parameter
    This is no longer used in the 3D mask algorithm.

--- src/nxrefine/scripts/nxtransform.py
+++ src/nxrefine/scripts/nxtransform.py
@@ -25,8 +25,6 @@ def main():
     parser.add_argument('-ql', nargs=3, help='Ql - min, step, max')
     parser.add_argument('-r', '--radius', default=200,
                         help='radius of mask around each peak (in pixels)')
-    parser.add_argument('-w', '--width', default=3,
-                        help='width of masked region (in frames)')
     parser.add_argument('-o', '--overwrite', action='store_true',
                         help='overwrite existing transforms')
     parser.add_argument('-q', '--queue', action='store_true',
@@ -41,8 +39,8 @@ def main():
 
     for entry in entries:
         reduce = NXReduce(
-            entry, args.directory, transform=True, mask=args.mask, Qh=args.qh,
-            Qk=args.qk, Ql=args.ql, radius=args.radius, width=args.width,
+            entry, args.directory, transform=True, mask=args.mask,
+            Qh=args.qh, Qk=args.qk, Ql=args.ql, radius=args.radius,
             overwrite=args.overwrite)
         if args.mask:
             if args.queue:
