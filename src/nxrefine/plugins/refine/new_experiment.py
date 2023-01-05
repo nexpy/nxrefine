8a2050ef60219812c4dec9a1860388c1c07f0df8
Ray Osborn <rayosborn@mac.com>
2023-01-04 19:00:05 -0600

    Add 'nxrefine' to experiment paths in CHESS

--- src/nxrefine/plugins/refine/new_experiment.py
+++ src/nxrefine/plugins/refine/new_experiment.py
@@ -11,6 +11,7 @@ from pathlib import Path
 from nexpy.gui.datadialogs import GridParameters, NXDialog
 from nexpy.gui.utils import report_error
 from nexusformat.nexus import NeXusError
+from nxrefine.nxbeamline import get_beamline
 from nxrefine.nxdatabase import NXDatabase
 
 
@@ -26,32 +27,38 @@ class ExperimentDialog(NXDialog):
 
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.parameters = GridParameters()
-        self.parameters.add('experiment', 'experiment', 'Experiment')
-
-        self.set_layout(
-            self.directorybox('Choose Home Directory', default=False),
-            self.parameters.grid(header=False),
-            self.close_layout(save=True))
+        self.beamline = get_beamline()
+        if self.beamline.name == 'QM2':
+            self.set_layout(
+                self.directorybox('Choose Home Directory', default=False),
+                self.close_layout(save=True))
+        else:        
+            self.parameters = GridParameters()
+            self.parameters.add('experiment', 'experiment', 'Experiment')
+            self.set_layout(
+                self.directorybox('Choose Home Directory', default=False),
+                self.parameters.grid(header=False),
+                self.close_layout(save=True))
         self.set_title('New Experiment')
 
     def accept(self):
         try:
             home_directory = Path(self.get_directory())
-            experiment_directory = home_directory.joinpath(
-                self.parameters['experiment'].value)
+            if self.beamline.name == 'QM2':
+                experiment_directory = home_directory / 'nxrefine'
+            else:
+                experiment_directory = (home_directory /
+                                        self.parameters['experiment'].value)
             experiment_directory.mkdir(exist_ok=True)
-            self.mainwindow.default_directory = experiment_directory
-            configuration_directory = experiment_directory.joinpath(
-                'configurations')
+            self.mainwindow.default_directory = str(experiment_directory)
+            configuration_directory = experiment_directory / 'configurations'
             configuration_directory.mkdir(exist_ok=True)
-            task_directory = experiment_directory.joinpath('tasks')
+            task_directory = experiment_directory / 'tasks'
             task_directory.mkdir(exist_ok=True)
-            _ = NXDatabase(task_directory.joinpath('nxdatabase.db'))
-            calibration_directory = experiment_directory.joinpath(
-                'calibrations')
+            _ = NXDatabase(str(task_directory / 'nxdatabase.db'))
+            calibration_directory = experiment_directory / 'calibrations'
             calibration_directory.mkdir(exist_ok=True)
-            script_directory = experiment_directory.joinpath('scripts')
+            script_directory = experiment_directory / 'scripts'
             script_directory.mkdir(exist_ok=True)
             super().accept()
         except Exception as error:
