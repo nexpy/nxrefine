bec7adf3c9f1d37dbc5248da250de035ba9fa7a7
Ray Osborn <rayosborn@mac.com>
2023-07-26 16:54:17 -0500

    Use a parameter table to define new experiments

--- src/nxrefine/plugins/refine/new_experiment.py
+++ src/nxrefine/plugins/refine/new_experiment.py
@@ -8,7 +8,7 @@
 
 from pathlib import Path
 
-from nexpy.gui.datadialogs import NXDialog
+from nexpy.gui.datadialogs import NXDialog, GridParameters
 from nexpy.gui.pyqt import QtWidgets
 from nexpy.gui.utils import report_error
 from nexpy.gui.widgets import NXLineEdit, NXPushButton
@@ -30,74 +30,62 @@ class ExperimentDialog(NXDialog):
 
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.beamline = get_beamline()
-        if self.beamline.raw_home.exists():
-            raw_text = str(self.beamline.raw_home.resolve())
-        else:
-            raw_text = None
-        self.raw_button = NXPushButton('Choose Raw Directory',
-                                       slot=self.choose_raw_directory)
-        self.raw_box = NXLineEdit(raw_text, parent=self, width=300)
-        self.raw_layout = self.make_layout(self.raw_button, self.raw_box)
-        if self.beamline.experiment_home.exists():
-            exp_text = str(self.beamline.experiment_home.resolve())
-        else:
-            exp_text = None
-        self.exp_button = NXPushButton('Choose Experiment Directory',
-                                       slot=self.choose_exp_directory)
-        self.exp_box = NXLineEdit(exp_text, parent=self, width=300)
-        self.exp_layout = self.make_layout(self.exp_button, self.exp_box)
-        self.set_layout(
-            self.raw_layout,
-            self.exp_layout,
-            self.close_layout(save=True))
+        settings = NXSettings().settings
+        self.parameters = GridParameters()
+        defaults = settings['instrument']
+        self.parameters.add('source', defaults['source'], 'Source Name')
+        self.parameters.add('instrument', defaults['instrument'], 'Instrument')
+        self.parameters.add('raw_home', defaults['raw_home'],
+                            'Raw Data Root Directory')
+        self.parameters.add('raw_path', defaults['raw_path'],
+                            'Raw Data Subdirectory')
+        self.parameters.add('analysis_home', defaults['analysis_home'],
+                            'Analysis Root Directory')
+        self.parameters.add('analysis_path', defaults['analysis_path'],
+                            'Analysis Subdirectory')
+        self.parameters.add('experiment', '', 'Name of Experiment')
+        self.set_layout(self.parameters.grid(header=False, width=200),
+                        self.close_layout(save=True))
         self.set_title('New Experiment')
-
-    def choose_raw_directory(self):
-        directory = QtWidgets.QFileDialog.getExistingDirectory(
-            self, 'Choose Raw Directory', self.raw_box.text())
-        if Path(directory).exists():
-            self.raw_box.setText(str(directory))
-        if not self.exp_box.text():
-            self.exp_box.setText(str(self.raw_directory))
-
-    @property
-    def raw_directory(self):
-        return Path(self.raw_box.text())
-
-    def choose_exp_directory(self):
-        directory = QtWidgets.QFileDialog.getExistingDirectory(
-            self, 'Choose Experiment Directory', self.exp_box.text())
-        if Path(directory).exists():
-            self.exp_box.setText(str(directory))
-
-    @property
-    def exp_directory(self):
-        if (self.checkbox['nxrefine'].isChecked()
-                and not self.exp_box.text().endswith('nxrefine')):
-            return Path(self.exp_box.text()) / 'nxrefine'
-        else:
-            return Path(self.exp_box.text())
 
     def accept(self):
         try:
-            experiment_path = self.exp_directory
-            raw_path = self.raw_directory
-            experiment_path.mkdir(exist_ok=True)
+            source = self.parameters['source'].value
+            instrument = self.parameters['instrument'].value
+            raw_home = self.parameters['raw_home'].value
+            raw_path = self.parameters['raw_path'].value
+            analysis_home = self.parameters['analysis_home'].value
+            analysis_path = self.parameters['analysis_path'].value
+            experiment = self.parameters['experiment'].value
+            if not Path(raw_home).exists():
+                raise NeXusError(f"'{raw_home}' does not exist")
+            if not analysis_home:
+                analysis_home = raw_home
+            elif not Path(analysis_home).exists():
+                raise NeXusError(f"'{analysis_home}' does not exist")
+            experiment_path = Path(analysis_home) / experiment
+            if analysis_path:
+                experiment_path = experiment_path / analysis_path
+            experiment_path.mkdir(parents=True, exist_ok=True)
             self.mainwindow.default_directory = str(experiment_path)
             configuration_directory = experiment_path / 'configurations'
             configuration_directory.mkdir(exist_ok=True)
             task_directory = experiment_path / 'tasks'
             task_directory.mkdir(exist_ok=True)
             _ = NXDatabase(task_directory / 'nxdatabase.db')
-            settings = NXSettings(task_directory)
-            settings.set('instrument', 'raw_path', str(raw_path))
-            settings.set('instrument', 'experiment_path', str(experiment_path))
-            settings.save()
             calibration_directory = experiment_path / 'calibrations'
             calibration_directory.mkdir(exist_ok=True)
             script_directory = experiment_path / 'scripts'
             script_directory.mkdir(exist_ok=True)
+            settings = NXSettings(task_directory)
+            settings.set('instrument', 'source', source)
+            settings.set('instrument', 'instrument', instrument)
+            settings.set('instrument', 'raw_home', raw_home)
+            settings.set('instrument', 'raw_path', raw_path)
+            settings.set('instrument', 'analysis_home', analysis_home)
+            settings.set('instrument', 'analysis_path', analysis_path)
+            settings.set('instrument', 'experiment', experiment)
+            settings.save()
             super().accept()
         except Exception as error:
             report_error("Defining New Experiment", error)
