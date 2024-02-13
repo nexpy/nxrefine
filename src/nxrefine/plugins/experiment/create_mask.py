4ecfa53162ac92309fbe279dadcf274ab6124fa5
Ray Osborn <rayosborn@mac.com>
2024-02-13 13:43:15 -0600

    Add ability to edit parameters

--- src/nxrefine/plugins/experiment/create_mask.py
+++ src/nxrefine/plugins/experiment/create_mask.py
@@ -53,15 +53,12 @@ class MaskDialog(NXDialog):
                                    self.action_buttons(
                                        ('Add Shape', self.add_shape)),
                                    self.shape_box))
-            self.insert_layout(3, self.shape_choice)
-            self.shape_choice.setVisible(False)
-            self.insert_layout(4,
-                               self.action_buttons(
-                                   ('Remove Shape', self.remove_shape)))
             self.pushbutton['Add Shape'].setCheckable(True)
         self.pixel_mask = self.entry['instrument/detector/pixel_mask'].nxvalue
         self.shape = self.pixel_mask.shape
         self.load_calibration()
+        self.plot_data()
+        self.activate()
 
     def load_calibration(self):
         if 'calibration' in self.entry['instrument']:
@@ -103,29 +100,51 @@ class MaskDialog(NXDialog):
         self.pv.ytab.flipped = True
         self.pv.draw()
 
+    def plot_limits(self):
+        return self.pv.xaxis.get_limits() + self.pv.yaxis.get_limits()
+
     def add_shape(self):
         if self.pushbutton['Add Shape'].isChecked():
             if self.shape_box.currentText() == 'Rectangle':
-                self.shape = NXrectangle(self.xc-50, self.yc-50, 100, 100,
-                                         border_tol=0.1,
-                                         plotview=self.pv,
-                                         resize=True, facecolor='r',
-                                         edgecolor='k',
-                                         linewidth=1, alpha=0.3)
+                self.shape = MaskRectangle(self.xc-50, self.yc-50, 100, 100,
+                                           parent=self,
+                                           border_tol=0.1,
+                                           plotview=self.pv,
+                                           resize=True, facecolor='r',
+                                           edgecolor='k',
+                                           linewidth=1, alpha=0.3)
             else:
-                self.shape = NXcircle(self.xc, self.yc, 50, border_tol=0.1,
-                                      plotview=self.pv, resize=True,
-                                      facecolor='r', edgecolor='k',
-                                      linewidth=1, alpha=0.3)
+                self.shape = MaskCircle(self.xc, self.yc, 50,
+                                        parent=self,
+                                        border_tol=0.1,
+                                        plotview=self.pv, resize=True,
+                                        facecolor='r', edgecolor='k',
+                                        linewidth=1, alpha=0.3)
+            self.shape.label = repr(self.shape)
             self.shape.connect()
+            self.shape_grid = self.shape_options(self.shape)
             self.pv.draw()
+            if len(self.shapes) == 0:
+                self.insert_layout(3, self.shape_choice)
+                self.shape_choice.setVisible(False)
+                self.insert_layout(4, self.shape_grid)
+                self.insert_layout(5,self.action_buttons(('Remove Shape',
+                                                          self.remove_shape)))
+            else:
+                self.insert_layout(4, self.shape_grid)
+            self.pushbutton['Add Shape'].setText('Save Shape')
         else:
+            self.shape.label = repr(self.shape)
             self.shapes.append(self.shape)
             self.pv.shapes.append(self.shapes)
-            self.shape_choice.addItem(repr(self.shape))
+            self.shape_choice.addItem(self.shape.label)
             self.shape_choice.setVisible(True)
-            self.insert_layout(self.shape_options(self.shape))
+            xmin, xmax, ymin, ymax = self.plot_limits()
             self.plot_data()
+            self.pv.set_plot_limits(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax)
+            self.pv.otab.zoom()
+            self.delete_grid(self.shape_grid)
+            self.pushbutton['Add Shape'].setText('Add Shape')
 
     def remove_shape(self):
         current_shape = self.shape_choice.selected
@@ -135,23 +154,58 @@ class MaskDialog(NXDialog):
                 self.shape_choice.remove(current_shape)
         if len(self.shapes) == 0:
             self.shape_choice.setVisible(False)
+        xmin, xmax, ymin, ymax = self.plot_limits()
         self.plot_data()
+        self.pv.set_plot_limits(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax)
+
+    def change_shape(self):
+        if isinstance(self.shape, NXrectangle):
+            x = self.parameters[self.shape]['x'].value
+            y = self.parameters[self.shape]['y'].value
+            w = self.parameters[self.shape]['w'].value
+            h = self.parameters[self.shape]['h'].value
+            self.shape.set_bounds(x, y, w, h)
+        else:
+            x = self.parameters[self.shape]['x'].value
+            y = self.parameters[self.shape]['y'].value
+            self.shape.set_center(x, y)
+            r = abs(self.parameters[self.shape]['r'].value)
+            self.shape.set_radius(r)
+        self.update_shape()
+
+    def update_shape(self):
+        if isinstance(self.shape, NXrectangle):
+            x, y = self.shape.xy
+            w, h = self.shape.width, self.shape.height
+            self.parameters[self.shape]['x'].value = x
+            self.parameters[self.shape]['y'].value = y
+            self.parameters[self.shape]['w'].value = w
+            self.parameters[self.shape]['h'].value = h
+        else:
+            x, y = self.shape.center
+            r = abs(self.shape.width) / 2
+            self.parameters[self.shape]['x'].value = x
+            self.parameters[self.shape]['y'].value = y
+            self.parameters[self.shape]['r'].value = r
+        self.shape.label = repr(self.shape)
+        self.shape_choice.setItemText(
+            self.shape_choice.findText(self.shape.label), repr(self.shape))               
 
     def shape_options(self, shape):
         p = self.parameters[shape] = GridParameters()
         if isinstance(shape, NXrectangle):
             x, y = shape.xy
             w, h = shape.width, shape.height
-            p.add('x', x, 'Left Pixel')
-            p.add('y', y, 'Bottom Pixel')
-            p.add('w', w, 'Width')
-            p.add('h', h, 'Height')
+            p.add('x', x, 'Left Pixel', slot=self.change_shape)
+            p.add('y', y, 'Bottom Pixel', slot=self.change_shape)
+            p.add('w', w, 'Width', slot=self.change_shape)
+            p.add('h', h, 'Height', slot=self.change_shape)
         else:
             x, y = shape.center
             r = abs(shape.width) / 2
-            p.add('x', x, 'X-Center')
-            p.add('y', y, 'Y-Center')
-            p.add('r', r, 'Radius')
+            p.add('x', x, 'X-Center', slot=self.change_shape)
+            p.add('y', y, 'Y-Center', slot=self.change_shape)
+            p.add('r', r, 'Radius', slot=self.change_shape)
         return p.grid(header=False)
 
     def create_mask(self):
@@ -188,9 +242,33 @@ class MaskDialog(NXDialog):
             return
         super().accept()
         if 'Mask Editor' in plotviews:
-            plotviews['Mask Editor'].close_view()
+            plotviews['Mask Editor'].close()
 
     def reject(self):
         super().reject()
         if 'Mask Editor' in plotviews:
-            plotviews['Mask Editor'].close_view()
+            plotviews['Mask Editor'].close()
+
+
+class MaskRectangle(NXrectangle):
+
+    def __init__(self, x, y, dx, dy, **kwargs):
+        self.parent = kwargs['parent']
+        del kwargs['parent']
+        super().__init__(x, y, dx, dy, **kwargs)
+
+    def update(self, x, y):
+        super().update(x, y)
+        self.parent.update_shape()
+
+
+class MaskCircle(NXcircle):
+
+    def __init__(self, x, y, r, **kwargs):
+        self.parent = kwargs['parent']
+        del kwargs['parent']
+        super().__init__(x, y, r, **kwargs)
+
+    def update(self, x, y):
+        super().update(x, y)
+        self.parent.update_shape()
