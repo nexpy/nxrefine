e8239a2e4285e45551a7ca383159861353379dca
Ray Osborn <rayosborn@mac.com>
2024-02-14 15:23:43 -0600

    Fixes the 'New Sample' dialog when the analysis path is defined

--- src/nxrefine/plugins/experiment/new_sample.py
+++ src/nxrefine/plugins/experiment/new_sample.py
@@ -12,6 +12,8 @@ from nexpy.gui.datadialogs import GridParameters, NXDialog
 from nexpy.gui.utils import report_error
 from nexusformat.nexus import NeXusError
 
+from nxrefine.nxsettings import NXSettings
+
 
 def show_dialog():
     try:
@@ -30,6 +32,10 @@ class SampleDialog(NXDialog):
         self.sample.add('sample', 'sample', 'Sample Name')
         self.sample.add('label', 'label', 'Sample Label')
 
+        settings = NXSettings().settings
+        self.default_directory = settings['instrument']['analysis_home']
+        self.analysis_path = settings['instrument']['analysis_path']
+
         self.set_layout(self.directorybox('Choose Experiment Directory',
                                           default=False), 
                         self.sample.grid(header=False),
@@ -38,8 +44,20 @@ class SampleDialog(NXDialog):
                         self.close_buttons(close=True))
         self.set_title('New Sample')
 
+    def choose_directory(self):
+        if self.default_directory:
+            self.set_default_directory(self.default_directory)
+        super().choose_directory()
+
+    @property
+    def experiment_directory(self):
+        directory = Path(self.get_directory())
+        if self.analysis_path and directory.name != self.analysis_path:
+            directory = directory / self.analysis_path
+        return directory
+
     def create_sample_directory(self):
-        self.sample_directory = (Path(self.get_directory()) /
+        self.sample_directory = (self.experiment_directory /
                                  self.sample['sample'].value /
                                  self.sample['label'].value)
         self.sample_directory.mkdir(parents=True, exist_ok=True)
