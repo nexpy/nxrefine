187a0ce2bd45d9fc08935a60140695c7ea382f09
Ray Osborn <rayosborn@mac.com>
2024-02-22 20:05:02 -0600

    Add autosection extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autosectionlabel']
 
 templates_path = ['_templates']
 exclude_patterns = []
