6adf6e5b1169df7f9981f0593cacaeb8f3f3ed62
Ray Osborn <rayosborn@mac.com>
2026-05-11 09:54:49 -0500

    Fix bug in resolving queue directories

--- src/nxrefine/nxreduce.py
+++ src/nxrefine/nxreduce.py
@@ -2069,7 +2069,7 @@ class NXReduce(QtCore.QObject):
 
         if args:
             if 'directory' in args:
-                args.directory = str(args.directory.resolve())
+                args.directory = str(Path(args.directory.resolve()))
             self.server.add_task(f"{command} {switches(args)}")
         else:
             self.server.add_task(
